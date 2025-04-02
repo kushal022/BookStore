61cc03e4c08b3b94e7f5bc148b1df2c9f1248549
kushal jangid <kushal.learn22@gmail.com>
2025-04-02 23:02:57 +0530

    All books routes created

--- server/app.js
+++ server/app.js
@@ -7,7 +7,8 @@ const PORT = process.env.PORT || 3000;
 // MDB
 require('./config/db')
 // Import Routes
-const userRoute = require('./routes/userRoute');
+const userRoutes = require('./routes/userRoute');
+const booksRoutes = require('./routes/bookRoute') 
 
 //middleware
 app.use(express.json()) // tells the formate of data
@@ -16,8 +17,10 @@ app.get('/api', (req,res)=>{
     res.send('home page')
 })
 
-// use Routes:
-app.use('/api/user',userRoute)
+// user Routes:
+app.use('/api/user',userRoutes)
+// Books Routes:
+app.use('/api/book',booksRoutes)
 //Start Server
 app.listen(PORT,()=>{
     console.log(`Server is listening at ${PORT}..`)
