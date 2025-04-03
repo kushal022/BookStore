e60a2e0bffe579402e76c013468f840e81a10acd
kushal jangid <kushal.learn22@gmail.com>
2025-04-03 09:59:16 +0530

    add and delete api for favorite books

--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const {authenticateToken} = require('./userAuth');
+const { authenticateToken } = require('./userAuth');
 
+const {addBookToFavoriteCtrl,
+    removeBookFromFavoriteCtrl,
+} = require('../controllers/favoriteCtrl')
 
+//todo------------------- Add Book to Favorite --------------------
+router.put('/addBookToFavorite', authenticateToken, addBookToFavoriteCtrl)
+//todo------------------- Remove Book From Favorite --------------------
+router.delete('/removeBookFromFavorite', authenticateToken, removeBookFromFavoriteCtrl)
 
 module.exports = router;
