d1191e6b8989596547533968b6a4258e9a48944f
kushal jangid <kushal.learn22@gmail.com>
2025-04-04 15:04:44 +0530

    home page  and some other  pages have  created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Navbar from './component/Navbar/Navbar'
 import Footer from './component/Footer/Footer'
+import Home from './pages/Home'
+import LogIn from './pages/Login'
+import SignUp from './pages/SignUp'
+import AllBooks from './pages/AllBooks'
+import AboutUs from './pages/AboutUs'
+import Cart from './pages/Cart'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Footer/>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route  path='/all-books' element={<AllBooks/>} />
+          <Route  path='/login' element={<LogIn/>} />
+          <Route  path='/signup' element={<SignUp/>} />
+          <Route  path='/about' element={<AboutUs/>} />
+          <Route  path='/cart' element={<Cart/>} />
+          <Route  path='/profile' element={<Profile/>} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   )
 }
