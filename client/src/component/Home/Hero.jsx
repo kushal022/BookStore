54ecbed36712f71055f328c4dff4de1ffd14592d
kushal jangid <kushal.learn22@gmail.com>
2025-04-04 17:10:24 +0530

    Mobile navbar done

--- client/src/component/Home/Hero.jsx
+++ client/src/component/Home/Hero.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Hero = () => {
   return (
-    <div className='h-[75vh] flex '>
-        <div className='w-full lg:w-3/6 flex flex-col items-center lg:items-start justify-center'>
+    <div className=' md:h-[77vh] flex flex-col md:flex-row items-center justify-center gap-4'>
+        <div className='w-full mb-12 lg:w-3/6 flex flex-col items-center lg:items-start justify-center'>
             <h1 className='text-4xl lg:text-6xl font-semibold text-yellow-100 text-center lg:text-left'>
                 Discover Your Next Great Read</h1>
             <p className='mt-4 text-xl text-zinc-300 text-center lg:text-left '>
@@ -11,9 +13,9 @@ const Hero = () => {
                 inspiration in our curated collection of books
             </p>
             <div className='mt-8'>
-            <button className='text-yellow-100 text-xl lg:text-2xl font-semibold border border-yellow-100 px-10 py-3 hover:bg-zinc-800 rounded-full'>
+            <Link to='/all-books' className='text-yellow-100 text-xl lg:text-2xl font-semibold border border-yellow-100 px-10 py-3 hover:bg-zinc-800 rounded-full'>
                 Discover Books
-            </button>
+            </Link>
             </div>
         </div>
         
