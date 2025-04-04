3cd1838113b0c11889ec817bf60a32c0df2935e4
kushal jangid <kushal.learn22@gmail.com>
2025-04-04 13:27:59 +0530

    footer UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './component/Navbar/Navbar'
+import Footer from './component/Footer/Footer'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Footer/>
     </div>
   )
 }
