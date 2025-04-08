8995481c6533968bfa6b6559f746f4703d7f802a
kushal jangid <kushal.learn22@gmail.com>
2025-04-08 13:32:16 +0530

    update 1.0.0.0

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import RecentlyAdded from '../component/Home/RecentlyAdded'
 
 const Home = () => {
   return (
-    <div className='bg-zinc-900 text-white px-10 py-8'>
+    <div className='bg-zinc-900 text-white px-4 md:px-10 py-8'>
         <Hero/>
         <RecentlyAdded/>
     </div>
